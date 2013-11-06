fa2b8c90221f4bd7dcabbfcdaa03d1ab292ce50b
Cesar Ramirez <ramirez@cesarandres.com>
2013-11-06 07:30:11 -0600

    Added resource folder

--- src/com/cesarandres/gnaw/FragmentProfile.java
+++ src/com/cesarandres/gnaw/FragmentProfile.java
@@ -65,7 +65,7 @@ public class FragmentProfile extends Fragment {
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		// Inflate the layout for this fragment
-		return inflater.inflate(R.layout.fragment_fragment_profile, container,
+		return inflater.inflate(R.layout.fragment_profile, container,
 				false);
 	}
 
