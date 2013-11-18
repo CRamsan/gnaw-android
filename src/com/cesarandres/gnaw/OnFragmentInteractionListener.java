a7bddbeb6432f2cb677d71cd8fababf48af8ef77
Cesar Ramirez <ramirez@cesarandres.com>
2013-11-18 10:47:59 -0600

    Added resources and fragments

---
+++ src/com/cesarandres/gnaw/OnFragmentInteractionListener.java
@@ -0,0 +1,8 @@
+package com.cesarandres.gnaw;
+
+import android.net.Uri;
+
+public interface OnFragmentInteractionListener {
+	// TODO: Update argument type and name
+	public void onFragmentInteraction(Uri uri);
+}
